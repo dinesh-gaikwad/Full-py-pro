e7561a7afc44fab6bcc0d919295394d80f37a005
dinesh-gaikwad <gaikwaddinesh0503@gmail.com>
2025-12-10 18:11:47 +0530

    Add Message model for chat application

---
+++ Module.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Message(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    room = models.CharField(max_length=100)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} @ {self.room}: {self.content[:20]}"
